6140a81257956c29d9909acf205422abeef0e737
VEDANG SINGH <64647602+ELIX3108@users.noreply.github.com>
2022-11-25 15:13:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Learning how to deploy on Heroku
-        </p>
+        <h1 style="text-align:center;">
+          Internal Lab Practical 
+        </h1>
+        <h2 style="text-align:center;">Cloud Computing</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
